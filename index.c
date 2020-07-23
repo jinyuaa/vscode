00f509348e4f919351695882891a8fbb57423aed
jinyuaa <596338260@qq.com>
2020-07-23 10:05:50 +0800

    add c python

---
+++ index.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int x = 4;
+    printf("%p, %d\n", &x, x);
+    x = 5;
+    printf("%p, %d\n", &x, x);
+    
+    return 0;
+}
---
+++ index.py
@@ -0,0 +1,4 @@
+x = 4
+print(id(x), x)
+x = 5
+print(id(x), x)
